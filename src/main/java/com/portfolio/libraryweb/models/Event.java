c6a14200a4cf38523fd75d1527f407584d7d645a
VahidOrVaxa <vagidzibluk@gmail.com>
2024-10-09 00:08:28 +0300

    Fixed date: from String type to java.util.Date. Add method to format output of data

--- src/main/java/com/portfolio/libraryweb/models/Event.java
+++ src/main/java/com/portfolio/libraryweb/models/Event.java
@@ -2,6 +2,8 @@ package com.portfolio.libraryweb.models;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 public class Event {
     @Id
@@ -9,14 +11,14 @@ public class Event {
     private long id;
     private String title;
     private String category;
-    private String eventDate;
+    private Date eventDate;
 
     @Column(columnDefinition = "varchar(3000)")
     private String text;
 
     public Event() {}
 
-    public Event(String title, String category, String eventDate, String text) {
+    public Event(String title, String category, Date eventDate, String text) {
         this.title = title;
         this.category = category;
         this.eventDate = eventDate;
@@ -55,11 +57,15 @@ public class Event {
         this.text = text;
     }
 
-    public String getEventDate() {
+    public Date getEventDate() {
         return eventDate;
     }
 
-    public void setEventDate(String dateOfCreation) {
+    public void setEventDate(Date dateOfCreation) {
         this.eventDate = dateOfCreation;
+    }
+
+    public String getFormattedEventDate() {
+        return String.format("Дата проведения: %1$td.%1$tm.%1$tY", eventDate);
     }
 }
