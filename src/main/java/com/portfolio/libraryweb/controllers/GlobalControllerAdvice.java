a148422f0254f17c580cc35306a8d7aa804da3d0
Zibliuc Vagid <vagidzibluk@gmail.com>
2024-10-20 19:07:49 +0300

    New feature: Add GlobalController that return avatar of authorized user in all templates, where exists variable 'userAvatar'

---
+++ src/main/java/com/portfolio/libraryweb/controllers/GlobalControllerAdvice.java
@@ -0,0 +1,24 @@
+package com.portfolio.libraryweb.controllers;
+
+import com.portfolio.libraryweb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+    @Autowired
+    private UserService userService;
+
+    @ModelAttribute("userAvatar")
+    public String userAvatar() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")) {
+            return null;
+        }
+        return userService.getBase64ImageString();
+    }
+
+}
